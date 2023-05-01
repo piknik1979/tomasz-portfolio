8b9c7af9df03482ccef52d6ff1e8fa5413824ad4
piknik1979 <tpikniczka@gmail.com>
2023-05-01 19:52:32 +0100

    murdle-project linked

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
-// import IMG3 from '../../assets/portfolio3.jpg'
+import IMG3 from '../../assets/portfolio3.png'
 // import IMG4 from '../../assets/portfolio4.jpg'
 // import IMG5 from '../../assets/portfolio5.png'
 // import IMG6 from '../../assets/portfolio6.jpg'
@@ -24,13 +24,13 @@ const data = [
     github: 'https://github.com/piknik1979/react-calc.git',
     demo: 'https://tomasz-react-calc-app.netlify.app/'
   },
-  // {
-  //   id: 3,
-  //   image: IMG3,
-  //   title: 'Figma dashboard UI kit for data design web apps',
-  //   github: 'https://github.com',
-  //   demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
-  // },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Wordle game clone application produced with collegues with Northcoders',
+    github: 'https://github.com/maximal-games/maximal-murdle',
+    demo: 'https://play.google.com/store/apps/details?id=com.maximal.maximalMurdle&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1&pli=1'
+  },
   // {
   //   id: 4,
   //   image: IMG4,
