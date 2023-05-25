9834eeda36c781c2909f3bcf9e88a364fe023d10
piknik1979 <tpikniczka@gmail.com>
2023-05-25 18:51:26 +0100

    About changed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
             <article className='about__card'>
               <VscFolderLibrary className='about__icon'/>
               <h5>Projects</h5>
-              <small>Completed:</small>
+              <small>Please see projects below</small>
             </article>
           </div>
 
